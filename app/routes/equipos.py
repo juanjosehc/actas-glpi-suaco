3bf161b978305ca9e539abbdc1111e06643068e3
juanjosehc <razortxz@gmail.com>
2026-07-29 11:48:41 -0500

    Java 2.2 "Eliminacion Python"

--- app/routes/equipos.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.services.glpi import buscar_equipo
-
-router = APIRouter()
-
-@router.get("/equipo/{serial}")
-def obtener_equipo(serial: str):
-
-    return buscar_equipo(serial)
